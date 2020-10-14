f1238aeaf083eca2578f536a0cd2001caad75da0
leo <lioryarkon9@gmail.com>
2020-10-15 00:11:29 +0300

    created mocks of 8 teams and all their players and updated 'Mocky' responses service

--- src/components/App.js
+++ src/components/App.js
@@ -6,33 +6,55 @@ import {
   Redirect,
 } from "react-router-dom";
 
-import players from "../premierLeaguePlayers.json";
 import teams from "../premierLeagueTeams.json";
+
+import arsenalStaff from "../staffs/arsenal.json";
+import astonVillaStaff from "../staffs/astonVilla.json";
+import brightonStaff from "../staffs/brighton.json";
+import burnleyStaff from "../staffs/bunley.json";
+import chelseaStaff from "../staffs/chelsea.json";
+import crystalPalaceStaff from "../staffs/crystalPalace.json";
+import evertonStaff from "../staffs/everton.json";
+import fulhamStaff from "../staffs/fulham.json";
 
 import Teams from "./Teams.page";
 import Team from "./Team.page";
 
-const footballTeams = teams.reduce((teamsById, currentTeam) => {
-  teamsById[currentTeam.id] = {
-    id: currentTeam.id,
-    name: currentTeam.name,
-    stadium: `${currentTeam.grounds[0].name}, ${currentTeam.grounds[0].city}`,
-    capacity: currentTeam.grounds[0].capacity,
-    websiteUrl: currentTeam.metadata.communities_URL,
-    logoId: currentTeam.altIds.opta,
-  };
+const appTeamsIds = [1, 2, 131, 43, 4, 6, 7, 34]
 
+const footballTeams = teams.reduce((teamsById, currentTeam) => {
+  if (appTeamsIds.includes(currentTeam.id)) {
+    teamsById[currentTeam.id] = {
+      id: currentTeam.id,
+      name: currentTeam.name,
+      stadium: `${currentTeam.grounds[0].name}, ${currentTeam.grounds[0].city}`,
+      capacity: currentTeam.grounds[0].capacity,
+      websiteUrl: currentTeam.metadata.communities_URL,
+      logoId: currentTeam.altIds.opta,
+    };
+  }
   return teamsById;
 }, {});
 
+const players = [
+  ...arsenalStaff.players.map(player => ({...player, teamId: arsenalStaff.team.id})),
+  ...astonVillaStaff.players.map(player => ({...player, teamId: astonVillaStaff.team.id})),
+  ...brightonStaff.players.map(player => ({...player, teamId: brightonStaff.team.id})),
+  ...burnleyStaff.players.map(player => ({...player, teamId: burnleyStaff.team.id})),
+  ...chelseaStaff.players.map(player => ({...player, teamId: chelseaStaff.team.id})),
+  ...crystalPalaceStaff.players.map(player => ({...player, teamId: crystalPalaceStaff.team.id})),
+  ...evertonStaff.players.map(player => ({...player, teamId: evertonStaff.team.id})),
+  ...fulhamStaff.players.map(player => ({...player, teamId: fulhamStaff.team.id}))
+]
+
 const footballPlayers = players.map((player) => ({
   shirtNumber: player.info.shirtNum,
-  teamId: player.currentTeam ? player.currentTeam.id : null,
+  teamId: player.teamId,
   name: `${player.name.first} ${player.name.last}`,
 }));
 
-console.log("teams:", footballTeams);
-console.log("players:", players);
+console.log(JSON.stringify(footballTeams));
+//console.log("players:", JSON.stringify(footballPlayers));
 
 const App = () => (
   <Router>
